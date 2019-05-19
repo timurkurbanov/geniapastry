51f674d10323dba1d43c96d481819ba88e130a5f
Timur Kurbanov <49075135+timurkurbanov@users.noreply.github.com>
2019-05-18 23:13:20 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // external js: flickity.pkgd.js
+
+const colors = ['#EE5335','white','#EE5335'];
+var blobs = document.querySelectorAll("#background path");
+
+blobs.forEach(blob => {
+    blob.style.fill = colors[Math.floor(Math.random() * colors.length)];
+});
