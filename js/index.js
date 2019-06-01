5e8d7f3bf79e3661b8ff56405a3ff3bac5a36428
Timur Kurbanov <49075135+timurkurbanov@users.noreply.github.com>
2019-06-01 13:45:41 -0400

    Delete index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// external js: flickity.pkgd.js
-
-const colors = ['#EE5335','white','#EE5335'];
-var blobs = document.querySelectorAll("#background path");
-
-blobs.forEach(blob => {
-    blob.style.fill = colors[Math.floor(Math.random() * colors.length)];
-});
-
